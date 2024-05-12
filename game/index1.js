0b5f238db6ba9e4d0b08223450979dce36c99c54
unknown <happinessxd743@gmail.com>
2024-05-12 15:52:29 +0545

    changes

--- game/index1.js
+++ game/index1.js
@@ -14,25 +14,62 @@ const xpText = document.querySelector("#xpText");
 const healthtext = document.querySelector("#healthtext");
 const cashtext = document.querySelector("#cashtext");
 
+const locations = [
+    {
+        name: "lobby",
+        "button text": ["buy 10 health(100 cash)","buy a weapon(100 cash)","go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "Welcome to the lobby"
+    },
+
+    {
+        name: "town square",
+        "button text": ["Go to lobby","Go to battle","Go to inventory"],
+        "button functions": [golobby, gobattle, goinventory],
+        text: " Welcome to the townsquare \"you can see the store\" "
+    }
+]
+
 //initialize buttons
 button1.onclick = golobby;
 button2.onclick = gobattle;
 button3.onclick = goinventory;
 
-function golobby() {
-button1.innerHTML = "buy 10 health(100 cash)";
-button2.innerHTML = "buy a weapon(100 cash)";
-button3.innerHTML = "go to town square";
+function update(location) {
+    button1.innerHTML = location["button text"][0];
+    button2.innerHTML = location["button text"][1];
+    button3.innerHTML = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerHTML = location.text;
 }
 
+function golobby() {
+    update(locations[0]);
+}
+
+function goTown() {
+    update(locations[1]);
+}
+
+function buyHealth() {
+    console.log("buy health");
+}
+
+function buyWeapon(){
+    console.log("buy weapon");
+}
+
+
 function gobattle() {
-console.log("Going into a battle");
-// Add your battle logic here
+    console.log("Going into a battle");
+    // Add your battle logic here
 }
 
 function goinventory() {
-console.log("Going to inventory");
-// Add your inventory logic here
+    console.log("Going to inventory");
+    // Add your inventory logic here
 }
 
 
